733f732a70e2f91c5fbf3740d10f792428e87048
FrancescoMazzocchi <123@email.it>
2023-12-20 16:14:16 +0100

    check ripetizioni con selettore

--- functions.php
+++ functions.php
@@ -8,33 +8,31 @@ function generaPass($alfabeto, $maiuscole, $numeri, $simboli)
         $n = rand(0, 3);
         if ($n == 0 && isset($_GET["letters"]) && $_GET["letters"]) {
             $char = $alfabeto[rand(0, count($alfabeto) - 1)];
-            if (!in_array($char, $usedCharacters)) {
+            if (!in_array($char, $usedCharacters) || $_GET["repeat"] == 'true') {
+                //se lascio solo il GET, aspettandomi quindi un valore true o false rimane sempre attivo. anche controllando un valore booleano
                 $password[] = $char;
                 $usedCharacters[] = $char;
                 $i++;
             }
         } else if ($n == 1 && isset($_GET["letters"]) && $_GET["letters"]) {
             $char = $maiuscole[rand(0, count($maiuscole) - 1)];
-            if (!in_array($char, $usedCharacters)) {
+            if (!in_array($char, $usedCharacters) || $_GET["repeat"] == 'true') {
                 $password[] = $char;
                 $usedCharacters[] = $char;
-
                 $i++;
             }
         } else if ($n == 2 && isset($_GET["numbers"]) && $_GET["numbers"]) {
             $char = $numeri[rand(0, count($numeri) - 1)];
-            if (!in_array($char, $usedCharacters)) {
+            if (!in_array($char, $usedCharacters) || $_GET["repeat"] == 'true') {
                 $password[] = $char;
                 $usedCharacters[] = $char;
-
                 $i++;
             }
         } else if ($n == 3 && isset($_GET["symbols"]) && $_GET["symbols"]) {
             $char = $simboli[rand(0, count($simboli) - 1)];
-            if (!in_array($char, $usedCharacters)) {
+            if (!in_array($char, $usedCharacters) || $_GET["repeat"] == 'true') {
                 $password[] = $char;
                 $usedCharacters[] = $char;
-
                 $i++;
             }
         }
